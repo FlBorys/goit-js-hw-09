28c4c822b91faa1f361d844c86a83ed025352772
FlBorys <131404105+FlBorys@users.noreply.github.com>
2023-09-24 01:15:58 +0300

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const changeColorStart = document.querySelector('[data-start]');
+const changeColorStop = document.querySelector('[data-stop]');
+const changeColorBackground = document.querySelector('body');
+changeColorStart.addEventListener('click', startSwitching);
+changeColorStop.addEventListener('click', stopSwitching);
+let timerId = null;
 
+
+function startSwitching() {
+    timerId = setInterval(() => {
+        changeColorBackground.style.backgroundColor = getRandomHexColor();
+        changeColorStart.disabled = true; 
+    }, 1000)
+}
+
+function stopSwitching() {
+    clearInterval(timerId);
+    changeColorStart.disabled = false;
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    
+}
